048f0cae1ed0984d883c5b2d31f166a6a2ccf0c5
Felipe Botelho Rodrigues <felipe.botelho@castgroup.com.br>
2021-12-06 11:49:30 -0300

    Created playlistAtom using recoil and show list of musics

--- components/Center.js
+++ components/Center.js
@@ -1,7 +1,11 @@
-import { useSession } from 'next-auth/react'
+import { signOut, useSession } from 'next-auth/react'
 import { ChevronDownIcon } from '@heroicons/react/outline'
 import { useEffect, useState } from 'react';
 import { shuffle } from 'lodash';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { playlistState, playlistIdState } from '../atoms/playlistAtom';
+import useSpotify from '../hooks/useSpotify';
+import Songs from './Songs';
 
 const colors = [
     "from-indigo-500",
@@ -15,24 +19,43 @@ const colors = [
 
 function Center() {
     const { data: session } = useSession()
+    const spotifyApi = useSpotify();
     const [color, setColor] = useState(null);
-    useEffect(()=>{
+    const playlistId = useRecoilValue(playlistIdState);
+    const [playlist, setPlaylist] = useRecoilState(playlistState)
+
+    useEffect(() => {
         setColor(shuffle(colors).pop());
-    },[])
+    }, [playlistId])
+
+    useEffect(() => {
+        spotifyApi.getPlaylist(playlistId).then((data) => {
+            setPlaylist(data.body)
+        }).catch(error => {
+            console.log(error);
+        });
+    }, [spotifyApi, playlistId])
 
     return (
-        <div className="flex-grow">
+        <div className="flex-grow h-screen overflow-y-scroll scrollbar-hide">
             <header className='absolute top-5 right-8'>
-                <div className='flex items-center bg-red-300 space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2'>
+                <div className='flex items-center bg-black space-x-3 text-white opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2'
+                onClick={signOut}>
                     <img className='rounded-full w-10 h-10' src={session?.user?.image} alt="" />
                     <h2>{session?.user?.name}</h2>
                     <ChevronDownIcon className='h-5 w-5' />
                 </div>
             </header>
             <section className={`flex items-end space-x-7 bg-gradient-to-b to-black ${color} h-80 text-white p-8`}>
-                {/* <img src=""/> */}
-                <h1>Hello</h1>
+                <img className='h-44 w-44 shadow-2xl' src={playlist?.images?.[0]?.url} />
+                <div>
+                    <p>PLAYLIST</p>
+                    <h1 className='text-2xl md:text-3xl xl:text-5xl font-bold'>{playlist?.name}</h1>
+                </div>
             </section>
+            <div>
+                <Songs/>
+            </div>
         </div>
     )
 }
--- components/Sidebar.js
+++ components/Sidebar.js
@@ -8,6 +8,8 @@ import {
 } from "@heroicons/react/outline"
 import { signOut, useSession } from 'next-auth/react'
 import { useEffect, useState } from "react";
+import { useRecoilState } from "recoil";
+import { playlistIdState } from "../atoms/playlistAtom";
 import useSpotify from "../hooks/useSpotify";
 
 function Sidebar() {
@@ -15,7 +17,7 @@ function Sidebar() {
     const spotifyApi = useSpotify();
     const { data: session, status } = useSession();
     const [playlists, setPlaylists] = useState([]);
-    const [playlistId, setPlaylistId] = useState(null);
+    const [playlistId, setPlaylistId] = useRecoilState(playlistIdState);
 
     useEffect(() => {
         debugger;
@@ -28,7 +30,7 @@ function Sidebar() {
 
     console.log(playlists)
     return (
-        <div className="text-gray-500 p-5 text-sm border-r border-gray-900 overflow-y-scroll scrollbar-hide h-screen">
+        <div className="text-gray-500 p-5 text-xs lg:text-sm border-r border-gray-900 overflow-y-scroll scrollbar-hide h-screen sm:max-w-[12rem] lg:max-w-[15rem] hidden md:inline-flex">
             <div className="space-y-4">
                 <button onClick={() => signOut()} className="flex items-center space-x-2 hover:text-white">
                     <p>Logout</p>
